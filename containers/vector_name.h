daa487b523701e1efd89f0c4c737d779af4ccba5
Kadda Aoues <houes_k@yahoo.fr>
2024-08-08 17:20:36 +0300

    this library will deleted

--- containers/vector_name.h
+++ containers/vector_name.h
@@ -4,30 +4,6 @@
 
 using namespace std;
 
-namespace Vec {
-
-class Vector :public vector<int>
-{
-	string m_Name ="";
-public:
-	using vector<int>::vector;
-
-	void push_front(int n){
-		push_back(0);
-		for (int i = size() - 1; i > 0; i--)
-			at(i) = at(i - 1);
-		at(0) = n;
-	}
-
-	void print() {
-		cout << "Vector " << m_Name <<" = ";
-		for (int i = 0; i < size(); i++) cout << at(i) << " ";
-		cout << endl;
-	}
-
-	void setName(const string name) { m_Name = name; }
-
-};
 
 vector<int> numberBase(int n, int k)
 {
